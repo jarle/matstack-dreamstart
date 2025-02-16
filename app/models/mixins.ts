baf63265e1400251ac0f0ddafbb4918d7502f292
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2025-02-16 06:54:52 +0100

    commit

--- app/models/mixins.ts
+++ app/models/mixins.ts
@@ -1,7 +1,7 @@
 import { NormalizeConstructor } from "@adonisjs/core/types/helpers";
 import { BaseModel, beforeCreate, column } from "@adonisjs/lucid/orm";
 import { nanoid } from "nanoid";
-import { v4 as uuid } from "uuid";
+import { randomUUID } from 'node:crypto';
 
 export function withUuid() {
   return <Model extends NormalizeConstructor<typeof BaseModel>>(source: Model) => {
@@ -17,7 +17,7 @@ export function withUuid() {
         modelInstance: InstanceType<T>
       ) {
         if (!modelInstance.id) {
-          modelInstance.id = uuid()
+          modelInstance.id = randomUUID()
         }
       }
     }
