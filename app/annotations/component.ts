1c3df7cccc4864ea5f81554d9b8ffe8ee4bf36d5
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2025-06-08 14:53:23 +0200

    commit

---
+++ app/annotations/component.ts
@@ -0,0 +1,5 @@
+import { inject } from '@adonisjs/core'
+
+export function Component() {
+  return inject()
+} 
---
+++ app/annotations/service.ts
@@ -0,0 +1,5 @@
+import { inject } from '@adonisjs/core'
+
+export function Service() {
+  return inject()
+} 
