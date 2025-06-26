a39b33aaffc4731307ab79aeacdb98c0a39df613
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2025-06-26 09:01:20 +0200

    inject env into services

--- app/services/_index.ts
+++ app/services/_index.ts
@@ -3,5 +3,6 @@ type LazyService<T = any> = () => Promise<{ default: T }>
 // Register services that should be available in the container here
 export const ServiceProviders = {
   email_login: () => import('./email_login_service.js'),
+  env: () => import('#start/env'),
   user_service: () => import('./user_service.js'),
 } satisfies Record<string, LazyService>
